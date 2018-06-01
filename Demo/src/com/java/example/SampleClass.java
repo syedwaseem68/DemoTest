09da534f8c15ba5bf33066a308ce0c65d527912e
User <User@DESKTOP-0SBO27J>
2018-06-01 22:57:09 +0530

    SampleClass

---
+++ Demo/src/com/java/example/SampleClass.java
@@ -0,0 +1,10 @@
+package com.java.example;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hiiiiiiiiiiiii");
+
+	}
+
+}
---
+++ Demo/src/com/java/example/Test.java
@@ -0,0 +1,10 @@
+package com.java.example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("helooooo");
+	}
+
+}
